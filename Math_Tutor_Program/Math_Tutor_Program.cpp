9e1b13d21670f9510c4d15a004d32ae0db342dfa
iStormathy <stormageddon473@yahoo.com>
2026-03-14 11:47:46 -0400

    Shortened variable names. Added notes. Added formatting

--- Math_Tutor_Program/Math_Tutor_Program.cpp
+++ Math_Tutor_Program/Math_Tutor_Program.cpp
@@ -4,6 +4,7 @@
 // before displaying the correct answer.
 #include <iostream>
 #include <random>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -11,15 +12,15 @@ int main()
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<> dist(1, 100);
-	int number1 = dist(gen);
-	int number2 = dist(gen);
-	int solution = number1 + number2;
-	int answer;
-	cout << "What is " << number1 << " + " << number2 << "?" << endl;
+	int num1 = dist(gen);
+	int num2 = dist(gen);
+	int solu = num1 + num2; // solution to the random equation
+	int answ; // allows user to input their answer
+	cout << "What is " << num1 << " + " << num2 << "?" << endl;
 	cout << "Take a moment to solve the equation, and type your answer when ready." << endl;
-	cin >> answer;
-	cout << "Your answer: " << answer << endl;
-	cout << "Correct answer: " << solution;
+	cin >> answ;
+	cout << "Your answer: " << setw(6) << answ << endl;
+	cout << "Correct answer: " << setw(3) <<solu;
 	
 	return 0;
 }
